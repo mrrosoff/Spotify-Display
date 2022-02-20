cd4aff9f71c7575507b5adfebaa8e29bb41b6c97
Matthew Ernst <matthew.f.ernst@gmail.com>
2022-02-20 13:06:45 -0700

    cleanup of loop()

--- SpotifyAPI/SpotifyAPI.ino
+++ SpotifyAPI/SpotifyAPI.ino
@@ -138,8 +138,7 @@ class WiFiUtilities {
         void checkWiFiModule() {
             if (WiFi.status() == WL_NO_MODULE) {
                 Serial.println("Communication With WiFi Module Failed!");
-                while (true)
-                ;
+                while (true);
             }
 
             String firmwareVersion = WiFi.firmwareVersion();
@@ -165,42 +164,54 @@ class WiFiUtilities {
 
 
 // SETUP & LOOP
+// TODO: Don't want in loop for redeclaring every time. Best Place??
+String clientId = "a9a84f65fc9f47568870f4c0c0185e3a"; 
+String clientSecret = "7cb7fe064e1844c19e87a2d475573948";
+String refreshToken = "AQAQL12IRdZORwdVF8sJeSiTrSNRVKbv1yZNqUFcfT6-ztpwK1gDjRFQZ0gQn5kBzKVL_3veFiHku1m8aDFYtThJzY9kjj3oX8_juhXTA9bn5_VSPHdqoSDPnsotBaEMkck"; 
+
+SpotifyWiFiClient spotifyClient(clientId, clientSecret, refreshToken);
+typedef SpotifyWiFiClient::ClientType ClientType;
+
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ;  // Wait For Serial Port to Connect.
-  }
-  WiFiUtilities wifiCheck("The Force", "thisistheway");
+  while (!Serial); // Wait For Serial Port to Connect.
+
+  String ssid = "The Force", password = "thisistheway";
+  WiFiUtilities wifiCheck(ssid, password);
   wifiCheck.checkWiFiModule();
   wifiCheck.connectToWiFi();
 
   
 }
 
-// TODO: Don't want in loop for redeclaring every time
-SpotifyWiFiClient spotifyClient("a9a84f65fc9f47568870f4c0c0185e3a", "7cb7fe064e1844c19e87a2d475573948", "AQAQL12IRdZORwdVF8sJeSiTrSNRVKbv1yZNqUFcfT6-ztpwK1gDjRFQZ0gQn5kBzKVL_3veFiHku1m8aDFYtThJzY9kjj3oX8_juhXTA9bn5_VSPHdqoSDPnsotBaEMkck");
-typedef SpotifyWiFiClient::ClientType ClientType;
-void loop() {
-
-    if(static_cast<long>(millis()) - spotifyClient.oauthExpiryTime > 0) {
+// Checking Clients Functions
+void oauthCheckAndRetrieve(SpotifyWiFiClient &spotifyClient) {
+  if(static_cast<long>(millis()) - spotifyClient.oauthExpiryTime > 0) {
+        Serial.println("Requesting OAuth Token");
         spotifyClient.getOAuthToken();
         spotifyClient.oauthExpiryTime = static_cast<long>(millis()) + 10000;
     }
+}
 
-    if(spotifyClient.oauthToken.length() > 0 && static_cast<long>(millis()) - spotifyClient.nextAlbumArtRetrievalTime > 0) {
+void albumArtURLCheckAndRetrieve(SpotifyWiFiClient &spotifyClient) {
+      if(spotifyClient.oauthToken.length() > 0 && static_cast<long>(millis()) - spotifyClient.nextAlbumArtRetrievalTime > 0) {
         Serial.println("Requesting Album Art");
         spotifyClient.getAlbumArtURL();
         spotifyClient.nextAlbumArtRetrievalTime = static_cast<long>(millis()) + 50000;
     }
+}
 
-    if(spotifyClient.albumArtURL.length() > 0 && static_cast<long>(millis()) - spotifyClient.nextDisplayUpdate > 0) {
+void albumArtCheckAndRetrieve(SpotifyWiFiClient &spotifyClient) {
+  if(spotifyClient.albumArtURL.length() > 0 && static_cast<long>(millis()) - spotifyClient.nextDisplayUpdate > 0) {
         Serial.println("Retrieving Album Art");
         spotifyClient.getAlbumArt();
         spotifyClient.nextDisplayUpdate = static_cast<long>(millis()) + 10000;
     }
+}
 
-    // OAUTH Client Loop
-    while(spotifyClient.currentClient == ClientType::OAUTH && spotifyClient.SSLClient.available()) {
+// Client Loops
+void oauthClientLoop(SpotifyWiFiClient &spotifyClient) {
+  while(spotifyClient.currentClient == ClientType::OAUTH && spotifyClient.SSLClient.available()) {
         if(!spotifyClient.checkHTTPStatus()) return;
         spotifyClient.skipHTTPHeaders();
 
@@ -217,9 +228,10 @@ void loop() {
         spotifyClient.nextClient();
         spotifyClient.SSLClient.stop();
     }
+}
 
-    // Album URL Client Loop
-    while(spotifyClient.currentClient == ClientType::ALBUM_URL && spotifyClient.SSLClient.available()) {
+void albumArtURLClientLoop(SpotifyWiFiClient &spotifyClient) {
+  while(spotifyClient.currentClient == ClientType::ALBUM_URL && spotifyClient.SSLClient.available()) {
         if (!spotifyClient.checkHTTPStatus()) return;
         spotifyClient.skipHTTPHeaders();
 
@@ -235,9 +247,10 @@ void loop() {
         spotifyClient.nextClient();
         spotifyClient.SSLClient.stop();
     }
+}
 
-    // Album Art Client Loop
-    while(spotifyClient.currentClient == ClientType::ALBUM_ART && spotifyClient.SSLClient.available()) {
+void albumArtClientLoop(SpotifyWiFiClient &spotifyClient) {
+  while(spotifyClient.currentClient == ClientType::ALBUM_ART && spotifyClient.SSLClient.available()) {
         // if (!checkHTTPStatus(albumArtClient)) return;
         // skipHTTPHeaders(albumArtClient);
 
@@ -254,6 +267,25 @@ void loop() {
         // spotifyClient.nextClient();
         // spotifyClient.SSLClient.stop();
     }
+}
+
+
+void loop() {
+
+    oauthCheckAndRetrieve(spotifyClient);
+    
+    albumArtURLCheckAndRetrieve(spotifyClient);
+
+    albumArtCheckAndRetrieve(spotifyClient);
+
+    // OAUTH Client Loop
+    oauthClientLoop(spotifyClient);
+
+    // Album URL Client Loop
+    albumArtURLClientLoop(spotifyClient);
+
+    // Album Art Client Loop
+    albumArtClientLoop(spotifyClient);
 
 }
 
